f51d09c8a5452b3b759687452f2cefda641e1e20
Joevano <myoshu.me@gmail.com>
2022-05-29 20:55:17 +0700

    initialize project

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { route } = require("express/lib/router");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = router;
